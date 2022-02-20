df04c6f2ddc4c497f9e3d3a6fc9dfe5a77a430fc
extremeheat <extreme@protonmail.ch>
2022-02-20 21:15:23 +0000

    start segfault-handler at top

--- binding.js
+++ binding.js
@@ -1,17 +1,18 @@
+const SegfaultHandler = require('segfault-handler')
 const helper = require('./helpers/buildPath.js')
 const fs = require('fs')
 const debug = require('debug')('raknet')
 
-if (!process.versions.electron) {
-  // Electron has its own crash handler, and segfault-handler
-  // uses NAN which is a hassle, so only load outside electron
-  try {
-    const SegfaultHandler = require('segfault-handler')
-    // SegfaultHandler.registerHandler('crash.log')
-  } catch (e) {
-    debug('[raknet] segfault handler is not installed. If you run into crashing issues, install it with `npm i -D segfault-handler` to get debug info on native crashes')
-  }
-}
+// if (!process.versions.electron) {
+//   // Electron has its own crash handler, and segfault-handler
+//   // uses NAN which is a hassle, so only load outside electron
+//   try {
+//     const SegfaultHandler = require('segfault-handler')
+//     // SegfaultHandler.registerHandler('crash.log')
+//   } catch (e) {
+//     debug('[raknet] segfault handler is not installed. If you run into crashing issues, install it with `npm i -D segfault-handler` to get debug info on native crashes')
+//   }
+// }
 
 let bindings
 const pathsToSearch = [helper.getPath()]
