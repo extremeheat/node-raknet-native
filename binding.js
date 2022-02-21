22a2a1dc76c57c9dfee8132af4f9feb1b2f96abb
extremeheat <extreme@protonmail.ch>
2022-02-20 22:20:48 -0500

    Update binding.js

--- binding.js
+++ binding.js
@@ -1,20 +1,18 @@
-const SegfaultHandler = require('segfault-handler');
-SegfaultHandler.registerHandler('crash.log');
-// SegfaultHandler.causeSegfault();
 const helper = require('./helpers/buildPath.js')
 const fs = require('fs')
 const debug = require('debug')('raknet')
 
-// if (!process.versions.electron) {
-//   // Electron has its own crash handler, and segfault-handler
-//   // uses NAN which is a hassle, so only load outside electron
-//   try {
-//     const SegfaultHandler = require('segfault-handler')
-//     // SegfaultHandler.registerHandler('crash.log')
-//   } catch (e) {
-//     debug('[raknet] segfault handler is not installed. If you run into crashing issues, install it with `npm i -D segfault-handler` to get debug info on native crashes')
-//   }
-// }
+if (!process.versions.electron) {
+  // Electron has its own crash handler, and segfault-handler
+  // uses NAN which is a hassle, so only load outside electron
+  // Note: Need to be using debug release to make use of this! Run `npm run clean` then `npm test` to get a debug build
+  try {
+    const SegfaultHandler = require('segfault-handler')
+    // SegfaultHandler.registerHandler('crash.log')
+  } catch (e) {
+    debug('[raknet] segfault handler is not installed. If you run into crashing issues, install it with `npm i -D segfault-handler` to get debug info on native crashes')
+  }
+}
 
 let bindings
 const pathsToSearch = [helper.getPath()]
